6d73a38ade1aba64c6fb3d258a232416cb41d262
fazarafi <faza.thirafi@gmail.com>
2017-11-30 01:55:15 +0700

    update dataset processing

--- fcm_cluster.py
+++ fcm_cluster.py
@@ -1,5 +1,6 @@
 from __future__ import division
 
+
 class Fcm_cluster(object):
 	def __init__(self, m, dataset, eps, num_cluster):
 		self.m = m
@@ -11,40 +12,59 @@ class Fcm_cluster(object):
 		self.m_matrix = [[0 for i in range(self.num_cluster)] for j in range(self.num_dataset)]
 
 	def init_matrix(self):
-		for i in range(0,self.num_dataset):
-			for j in range(0,self.num_cluster):
-				self.m_matrix[i][j] = 0.1
+		for i in range(self.num_dataset):
+			for j in range(self.num_cluster):
+				self.m_matrix[i][j] = 0.1*j+0.2
 		
 	def iterate(self):
-		centroids = [0 for i in range(self.num_cluster)]
+		centroids = [[] for i in range(self.num_cluster)]
 		stop = False
 		while not(stop):
 			prev_mat = self.m_matrix
-			for j in range(0,self.num_cluster):
-				sig_data_md = 1
-				sig_md = 1
-				for i in range(0,self.num_dataset):
-					sig_data_md += (self.m_matrix[i][j]**self.m)*self.dataset[i]
+			for j in range(self.num_cluster):
+				sig_data_md = []
+				sig_md = 0
+				for i in range(self.num_dataset):
+					sig_data_md.append(self.time_att_md(self.dataset[i],(self.m_matrix[i][j]**self.m)))
 					sig_md += self.m_matrix[i][j]**self.m
+				
+				centroids[j] = self.divide_att_md(self.accumulate_att(sig_data_md),sig_md)
 
-				centroids[j] = sig_data_md/sig_md
-			print(centroids)
-			
 			self.update_matrix(centroids)
+			# print('max=',self.find_max_md())
+			# print('eps=',self.eps)
 			if (self.find_max_md()<self.eps):
 				stop = True	
 			stop = True
 
 
-	def calc_centroid(self):
-		return ''
+	def time_att_md(self, attributes, md):
+		result = []
+		for i in range(len(attributes)):
+			result.append(attributes[i]*md)
+		return result
+
+	def divide_att_md(self, attributes, md):
+		result = []
+		for i in range(len(attributes)):
+			result.append(attributes[i]/md)
+		return result		
+
+	def accumulate_att(self, arr):
+		if len(arr)==0:
+			return []
+		else:
+			result = [0 for i in range(len(arr[0]))]
+			for i in range(len(arr)):
+				for j in range(len(arr[0])):
+					result[j] += arr[i][j]
+			return result
 
 	def update_matrix(self, centroids):	
-		print('update')
-		for i in range(0,self.num_dataset):
-			for j in range(0,self.num_cluster):
+		for i in range(self.num_dataset):
+			for j in range(self.num_cluster):
 				sig_dist = 0
-				for k in range(0,self.num_cluster):
+				for k in range(self.num_cluster):
 					sig_dist += (self.calc_dist(self.dataset[i],centroids[j])/
 						self.calc_dist(self.dataset[i],centroids[k]))**(2/(self.m-1))
 
@@ -53,8 +73,8 @@ class Fcm_cluster(object):
 
 	def find_max_md(self):
 		max_md = 0
-		for i in range(0,self.num_dataset):
-			for j in range(0,self.num_cluster):
+		for i in range(self.num_dataset):
+			for j in range(self.num_cluster):
 				if (self.m_matrix[i][j] > max_md):
 					max_md = self.m_matrix[i][j]
 
@@ -69,13 +89,16 @@ class Fcm_cluster(object):
 	
 	def calc_dist(self, data, centroid):
 		# TODO: cara cari distance
+
 		return 0.5
 
 
-fcm = Fcm_cluster(m=2, dataset=[0,2,3,2,1], eps=0.01, num_cluster=2)
+fcm = Fcm_cluster(m=2, dataset=[[1,2,3],[1,2,2]], eps=0.01, num_cluster=2)
 fcm.main_process()
-
-
+# arr = [[1,2,3],[1,2,2]]
+# for i in range(len(arr)):
+# 	print(i)
+# print(arr[0][1]+arr[1][1])
 
 
 
